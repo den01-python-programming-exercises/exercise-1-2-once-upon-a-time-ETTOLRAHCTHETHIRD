0ef824269c31228ece4b36e0b8eb2f554740fe92
ETTOLRAHCTHETHIRD <91137493+ETTOLRAHCTHETHIRD@users.noreply.github.com>
2021-09-21 15:02:07 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,4 +4,4 @@ from src.exercise import main
 def test_exercise(capsys):
     main()
     out, err = capsys.readouterr()
-    assert out == "Once upon a time\nthere was\na program\n", "Should read:\n 'Once upon a time\nthere was\na program\n'"
+    assert out == "Once upon aijhiu hi time\nthere was\na program\n", "Should read:\n 'Once upon a time\nthere was\na program\n'"
